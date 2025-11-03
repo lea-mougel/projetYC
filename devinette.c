a340d594706d54d48ecb1bca7bd17914c765b7e2
Léa MOUGEL <44008242@parisnanterre.fr>
2025-11-03 16:54:58 +0100

    v1 devinette

--- devinette.c
+++ devinette.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+// Nombre à deviner (entre 0 et 100)
+const int nombre_secret = 17;
+
 int main() {
-    printf("Hello World\n");
+    int nb_utilisateur;
+    
+    printf("Devinez un nombre entre 0 et 100 : ");
+    scanf("%d", &nb_utilisateur);
+    
+    if (nb_utilisateur == nombre_secret) {
+        printf("Bravo! Vous avez trouvé le nombre!\n");
+    } else {
+        printf("Désolé, ce n'est pas le bon nombre. Essayez encore!\n");
+    }
     return 0;
 }
